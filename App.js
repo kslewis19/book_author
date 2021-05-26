16155c616a2a1db95a6566630a3ad265ca6c3400
kslewis19 <60162532+kslewis19@users.noreply.github.com>
2021-05-26 17:40:00 -0400

    moved api key

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 9000
+const cors = require('cors')
+require('dotenv').config()
+const API_KEY = process.env.REST_API_KEY
+
+app.use(cors())
 
 const authorData = {
     "it": "stephan king"
@@ -10,8 +15,23 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.get('/book', (req, res) => {
+    const bookTitle = req.query.title
 
-    res.send(authorData.req.query.title)
+
+    const fetchAuthor = () => {
+        const url = `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&key=${API_KEY}`;
+        const axios = require('axios');
+        axios.get(url)
+            .then(response => {
+                console.log(response.data);
+
+            }, error => {
+                console.log(error);
+            });
+
+    }
+    fetchAuthor()
+    res.json({ author: authorData[bookTitle] })
 })
 
 app.listen(port, () => {
